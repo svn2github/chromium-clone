kuen-san/webrtc

commit 06745ba6bfa1efa41bfed404f24f13610dfbb2ed

Add field to counters for when first rtp/rtcp packet is sent/received. Use this time for histogram statistics (send/receive bitrates, sent/received rtcp fir/nack packets/min).
BUG=crbug/419657
R=mflodman@webrtc.org, stefan@webrtc.org

Review URL: https://webrtc-codereview.appspot.com/32219004

git-svn-id: http://webrtc.googlecode.com/svn/trunk/webrtc@7910 4adac7df-926f-26a2-2b94-8c16560cd09d

common_types.h
@@ -200,7 +200,8 @@ class RtcpStatisticsCallback {
 // Statistics for RTCP packet types.
 struct RtcpPacketTypeCounter {
   RtcpPacketTypeCounter()
-    : nack_packets(0),
+    : first_packet_time_ms(-1),
+      nack_packets(0),
       fir_packets(0),
       pli_packets(0),
       nack_requests(0),
@@ -212,6 +213,16 @@ struct RtcpPacketTypeCounter {
     pli_packets += other.pli_packets;
     nack_requests += other.nack_requests;
     unique_nack_requests += other.unique_nack_requests;
+    if (other.first_packet_time_ms != -1 &&
+       (other.first_packet_time_ms < first_packet_time_ms ||
+        first_packet_time_ms == -1)) {
+      // Use oldest time.
+      first_packet_time_ms = other.first_packet_time_ms;
+    }
+  }
+
+  int64_t TimeSinceFirstPacketInMs(int64_t now_ms) const {
+    return (first_packet_time_ms == -1) ? -1 : (now_ms - first_packet_time_ms);
   }
 
   int UniqueNackRequestsInPercent() const {
@@ -222,25 +233,27 @@ struct RtcpPacketTypeCounter {
         (unique_nack_requests * 100.0f / nack_requests) + 0.5f);
   }
 
-  uint32_t nack_packets;          // Number of RTCP NACK packets.
-  uint32_t fir_packets;           // Number of RTCP FIR packets.
-  uint32_t pli_packets;           // Number of RTCP PLI packets.
-  uint32_t nack_requests;         // Number of NACKed RTP packets.
+  int64_t first_packet_time_ms;  // Time when first packet is sent/received.
+  uint32_t nack_packets;   // Number of RTCP NACK packets.
+  uint32_t fir_packets;    // Number of RTCP FIR packets.
+  uint32_t pli_packets;    // Number of RTCP PLI packets.
+  uint32_t nack_requests;  // Number of NACKed RTP packets.
   uint32_t unique_nack_requests;  // Number of unique NACKed RTP packets.
 };
 
 // Data usage statistics for a (rtp) stream.
 struct StreamDataCounters {
   StreamDataCounters()
-   : bytes(0),
-     header_bytes(0),
-     padding_bytes(0),
-     packets(0),
-     retransmitted_bytes(0),
-     retransmitted_header_bytes(0),
-     retransmitted_padding_bytes(0),
-     retransmitted_packets(0),
-     fec_packets(0) {}
+    : first_packet_time_ms(-1),
+      bytes(0),
+      header_bytes(0),
+      padding_bytes(0),
+      packets(0),
+      retransmitted_bytes(0),
+      retransmitted_header_bytes(0),
+      retransmitted_padding_bytes(0),
+      retransmitted_packets(0),
+      fec_packets(0) {}
 
   void Add(const StreamDataCounters& other) {
     bytes += other.bytes;
@@ -252,6 +265,16 @@ struct StreamDataCounters {
     retransmitted_padding_bytes += other.retransmitted_padding_bytes;
     retransmitted_packets += other.retransmitted_packets;
     fec_packets += other.fec_packets;
+    if (other.first_packet_time_ms != -1 &&
+       (other.first_packet_time_ms < first_packet_time_ms ||
+        first_packet_time_ms == -1)) {
+      // Use oldest time.
+      first_packet_time_ms = other.first_packet_time_ms;
+    }
+  }
+
+  int64_t TimeSinceFirstPacketInMs(int64_t now_ms) const {
+    return (first_packet_time_ms == -1) ? -1 : (now_ms - first_packet_time_ms);
   }
 
   size_t TotalBytes() const {
@@ -268,6 +291,7 @@ struct StreamDataCounters {
   }
 
   // TODO(pbos): Rename bytes -> media_bytes.
+  int64_t first_packet_time_ms;  // Time when first packet is sent/received.
   size_t bytes;  // Payload bytes, excluding RTP headers and padding.
   size_t header_bytes;  // Number of bytes used by RTP headers.
   size_t padding_bytes;  // Number of padding bytes.
@@ -288,7 +312,7 @@ class StreamDataCountersCallback {
                                    uint32_t ssrc) = 0;
 };
 
-// Rate statistics for a stream
+// Rate statistics for a stream.
 struct BitrateStatistics {
   BitrateStatistics() : bitrate_bps(0), packet_rate(0), timestamp_ms(0) {}
 
@@ -307,7 +331,7 @@ class BitrateStatisticsObserver {
                       uint32_t ssrc) = 0;
 };
 
-// Callback, used to notify an observer whenever frame counts have been updated
+// Callback, used to notify an observer whenever frame counts have been updated.
 class FrameCountObserver {
  public:
   virtual ~FrameCountObserver() {}

modules/rtp_rtcp/source/receive_statistics_impl.cc
@@ -96,6 +96,7 @@ void StreamStatisticianImpl::UpdateCounters(const RTPHeader& header,
 
   if (receive_counters_.packets == 1) {
     received_seq_first_ = header.sequenceNumber;
+    receive_counters_.first_packet_time_ms = clock_->TimeInMilliseconds();
   }
 
   // Count only the new packets received. That is, if packets 1, 2, 3, 5, 4, 6

modules/rtp_rtcp/source/receive_statistics_unittest.cc
@@ -140,12 +140,18 @@ TEST_F(ReceiveStatisticsTest, GetReceiveStreamDataCounters) {
 
   StreamDataCounters counters;
   statistician->GetReceiveStreamDataCounters(&counters);
+  EXPECT_GT(counters.first_packet_time_ms, -1);
   EXPECT_EQ(1u, counters.packets);
 
-  // GetReceiveStreamDataCounters includes reset counter values.
   statistician->ResetStatistics();
+  // GetReceiveStreamDataCounters includes reset counter values.
+  statistician->GetReceiveStreamDataCounters(&counters);
+  EXPECT_GT(counters.first_packet_time_ms, -1);
+  EXPECT_EQ(1u, counters.packets);
+
   receive_statistics_->IncomingPacket(header1_, kPacketSize1, false);
   statistician->GetReceiveStreamDataCounters(&counters);
+  EXPECT_GT(counters.first_packet_time_ms, -1);
   EXPECT_EQ(2u, counters.packets);
 }
 

modules/rtp_rtcp/source/rtp_sender.cc
@@ -907,6 +907,9 @@ void RTPSender::UpdateRtpStats(const uint8_t* buffer,
 
   total_bitrate_sent_.Update(packet_length);
   ++counters->packets;
+  if (counters->packets == 1) {
+    counters->first_packet_time_ms = clock_->TimeInMilliseconds();
+  }
   if (IsFecPacket(buffer, header)) {
     ++counters->fec_packets;
   }

video_engine/vie_channel.cc
@@ -147,9 +147,7 @@ ViEChannel::ViEChannel(int32_t channel_id,
       sender_(sender),
       nack_history_size_sender_(kSendSidePacketHistorySize),
       max_nack_reordering_threshold_(kMaxPacketAgeToNack),
-      pre_render_callback_(NULL),
-      start_ms_(Clock::GetRealTimeClock()->TimeInMilliseconds()),
-      start_send_ms_(0) {
+      pre_render_callback_(NULL) {
   RtpRtcp::Configuration configuration = CreateRtpRtcpConfiguration();
   configuration.remote_bitrate_estimator = remote_bitrate_estimator;
   configuration.receive_statistics = vie_receiver_.GetReceiveStatistics();
@@ -237,76 +235,80 @@ ViEChannel::~ViEChannel() {
 }
 
 void ViEChannel::UpdateHistograms() {
-  // TODO(asapersson): Use time from first sent/received packet.
-  float elapsed_minutes =
-      (Clock::GetRealTimeClock()->TimeInMilliseconds() - start_ms_) / 60000.0f;
-  if (elapsed_minutes < metrics::kMinRunTimeInSeconds / 60.0f) {
-    return;
-  }
+  int64_t now = Clock::GetRealTimeClock()->TimeInMilliseconds();
   RtcpPacketTypeCounter rtcp_sent;
   RtcpPacketTypeCounter rtcp_received;
   GetRtcpPacketTypeCounters(&rtcp_sent, &rtcp_received);
 
   if (sender_) {
-    if (rtcp_received.nack_requests > 0) {
-      RTC_HISTOGRAM_PERCENTAGE(
-          "WebRTC.Video.UniqueNackRequestsReceivedInPercent",
-              rtcp_received.UniqueNackRequestsInPercent());
+    int64_t elapsed_sec = rtcp_received.TimeSinceFirstPacketInMs(now) / 1000;
+    if (elapsed_sec > metrics::kMinRunTimeInSeconds) {
+      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.NackPacketsReceivedPerMinute",
+          rtcp_received.nack_packets / elapsed_sec / 60);
+      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.FirPacketsReceivedPerMinute",
+          rtcp_received.fir_packets / elapsed_sec / 60);
+      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.PliPacketsReceivedPerMinute",
+          rtcp_received.pli_packets / elapsed_sec / 60);
+      if (rtcp_received.nack_requests > 0) {
+        RTC_HISTOGRAM_PERCENTAGE(
+            "WebRTC.Video.UniqueNackRequestsReceivedInPercent",
+                rtcp_received.UniqueNackRequestsInPercent());
+      }
     }
-    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.NackPacketsReceivedPerMinute",
-        rtcp_received.nack_packets / elapsed_minutes);
-    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.FirPacketsReceivedPerMinute",
-        rtcp_received.fir_packets / elapsed_minutes);
-    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.PliPacketsReceivedPerMinute",
-        rtcp_received.pli_packets / elapsed_minutes);
   } else if (vie_receiver_.GetRemoteSsrc() > 0) {
     // Get receive stats if we are receiving packets, i.e. there is a remote
     // ssrc.
-    if (rtcp_sent.nack_requests > 0) {
-      RTC_HISTOGRAM_PERCENTAGE("WebRTC.Video.UniqueNackRequestsSentInPercent",
-          rtcp_sent.UniqueNackRequestsInPercent());
+    int64_t elapsed_sec = rtcp_sent.TimeSinceFirstPacketInMs(now) / 1000;
+    if (elapsed_sec > metrics::kMinRunTimeInSeconds) {
+      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.NackPacketsSentPerMinute",
+          rtcp_sent.nack_packets / elapsed_sec / 60);
+      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.FirPacketsSentPerMinute",
+          rtcp_sent.fir_packets / elapsed_sec / 60);
+      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.PliPacketsSentPerMinute",
+          rtcp_sent.pli_packets / elapsed_sec / 60);
+      if (rtcp_sent.nack_requests > 0) {
+        RTC_HISTOGRAM_PERCENTAGE("WebRTC.Video.UniqueNackRequestsSentInPercent",
+            rtcp_sent.UniqueNackRequestsInPercent());
+      }
     }
-    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.NackPacketsSentPerMinute",
-        rtcp_sent.nack_packets / elapsed_minutes);
-    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.FirPacketsSentPerMinute",
-        rtcp_sent.fir_packets / elapsed_minutes);
-    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.PliPacketsSentPerMinute",
-        rtcp_sent.pli_packets / elapsed_minutes);
 
     StreamDataCounters rtp;
     StreamDataCounters rtx;
     GetReceiveStreamDataCounters(&rtp, &rtx);
     StreamDataCounters rtp_rtx = rtp;
     rtp_rtx.Add(rtx);
-    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.BitrateReceivedInKbps",
-        rtp_rtx.TotalBytes() * 8 / (elapsed_minutes * 60) / 1000);
-    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.MediaBitrateReceivedInKbps",
-        rtp.MediaPayloadBytes() * 8 / (elapsed_minutes * 60) / 1000);
-    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.PaddingBitrateReceivedInKbps",
-        rtp_rtx.padding_bytes * 8 / (elapsed_minutes * 60) / 1000);
-    RTC_HISTOGRAM_COUNTS_10000(
-        "WebRTC.Video.RetransmittedBitrateReceivedInKbps",
-            rtp_rtx.RetransmittedBytes() * 8 / (elapsed_minutes * 60) / 1000);
-    uint32_t ssrc = 0;
-    if (vie_receiver_.GetRtxSsrc(&ssrc)) {
-      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.RtxBitrateReceivedInKbps",
-          rtx.TotalBytes() * 8 / (elapsed_minutes * 60) / 1000);
+    elapsed_sec = rtp_rtx.TimeSinceFirstPacketInMs(now) / 1000;
+    if (elapsed_sec > metrics::kMinRunTimeInSeconds) {
+      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.BitrateReceivedInKbps",
+          rtp_rtx.TotalBytes() * 8 / elapsed_sec / 1000);
+      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.MediaBitrateReceivedInKbps",
+          rtp.MediaPayloadBytes() * 8 / elapsed_sec / 1000);
+      RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.PaddingBitrateReceivedInKbps",
+          rtp_rtx.padding_bytes * 8 / elapsed_sec / 1000);
+      RTC_HISTOGRAM_COUNTS_10000(
+          "WebRTC.Video.RetransmittedBitrateReceivedInKbps",
+              rtp_rtx.RetransmittedBytes() * 8 / elapsed_sec / 1000);
+      uint32_t ssrc = 0;
+      if (vie_receiver_.GetRtxSsrc(&ssrc)) {
+        RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.RtxBitrateReceivedInKbps",
+            rtx.TotalBytes() * 8 / elapsed_sec / 1000);
+      }
     }
   }
 }
 
 void ViEChannel::UpdateHistogramsAtStopSend() {
-  // TODO(asapersson): Use time from first sent packet.
-  int64_t elapsed_sec =
-      (Clock::GetRealTimeClock()->TimeInMilliseconds() - start_send_ms_) / 1000;
-  if (elapsed_sec < metrics::kMinRunTimeInSeconds) {
-    return;
-  }
   StreamDataCounters rtp;
   StreamDataCounters rtx;
   GetSendStreamDataCounters(&rtp, &rtx);
   StreamDataCounters rtp_rtx = rtp;
   rtp_rtx.Add(rtx);
+
+  int64_t elapsed_sec = rtp_rtx.TimeSinceFirstPacketInMs(
+      Clock::GetRealTimeClock()->TimeInMilliseconds()) / 1000;
+  if (elapsed_sec < metrics::kMinRunTimeInSeconds) {
+    return;
+  }
   RTC_HISTOGRAM_COUNTS_100000("WebRTC.Video.BitrateSentInKbps",
       rtp_rtx.TotalBytes() * 8 / elapsed_sec / 1000);
   RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.MediaBitrateSentInKbps",
@@ -1393,7 +1395,6 @@ int32_t ViEChannel::StartSend() {
     rtp_rtcp->SetSendingMediaStatus(true);
     rtp_rtcp->SetSendingStatus(true);
   }
-  start_send_ms_ = Clock::GetRealTimeClock()->TimeInMilliseconds();
   return 0;
 }
 

modules/rtp_rtcp/source/rtcp_receiver.cc
@@ -316,6 +316,10 @@ RTCPReceiver::IncomingRTCPPacket(RTCPPacketInformation& rtcpPacketInformation,
 
     _lastReceived = _clock->TimeInMilliseconds();
 
+    if (packet_type_counter_.first_packet_time_ms == -1) {
+      packet_type_counter_.first_packet_time_ms = _lastReceived;
+    }
+
     RTCPUtility::RTCPPacketTypes pktType = rtcpParser->Begin();
     while (pktType != RTCPUtility::kRtcpNotValidCode)
     {

modules/rtp_rtcp/source/rtcp_sender.cc
@@ -1632,6 +1632,10 @@ int RTCPSender::PrepareRTCP(const FeedbackState& feedback_state,
 
   CriticalSectionScoped lock(_criticalSectionRTCPSender);
 
+  if (packet_type_counter_.first_packet_time_ms == -1) {
+    packet_type_counter_.first_packet_time_ms = _clock->TimeInMilliseconds();
+  }
+
   if(_TMMBR )  // Attach TMMBR to send and receive reports.
   {
       rtcpPacketTypeFlags |= kRtcpTmmbr;

modules/rtp_rtcp/source/rtp_rtcp_impl_unittest.cc
@@ -325,16 +325,21 @@ TEST_F(RtpRtcpImplTest, RttForReceiverOnly) {
 }
 
 TEST_F(RtpRtcpImplTest, RtcpPacketTypeCounter_Nack) {
+  EXPECT_EQ(-1, receiver_.RtcpSent().first_packet_time_ms);
+  EXPECT_EQ(-1, sender_.RtcpReceived().first_packet_time_ms);
   EXPECT_EQ(0U, sender_.RtcpReceived().nack_packets);
   EXPECT_EQ(0U, receiver_.RtcpSent().nack_packets);
+
   // Receive module sends a NACK.
   const uint16_t kNackLength = 1;
   uint16_t nack_list[kNackLength] = {123};
   EXPECT_EQ(0, receiver_.impl_->SendNACK(nack_list, kNackLength));
   EXPECT_EQ(1U, receiver_.RtcpSent().nack_packets);
+  EXPECT_GT(receiver_.RtcpSent().first_packet_time_ms, -1);
 
   // Send module receives the NACK.
   EXPECT_EQ(1U, sender_.RtcpReceived().nack_packets);
+  EXPECT_GT(sender_.RtcpReceived().first_packet_time_ms, -1);
 }
 
 TEST_F(RtpRtcpImplTest, RtcpPacketTypeCounter_FirAndPli) {
@@ -353,6 +358,46 @@ TEST_F(RtpRtcpImplTest, RtcpPacketTypeCounter_FirAndPli) {
   // Send module receives the FIR and PLI.
   EXPECT_EQ(2U, sender_.RtcpReceived().fir_packets);
   EXPECT_EQ(1U, sender_.RtcpReceived().pli_packets);
+}
+
+TEST_F(RtpRtcpImplTest, AddStreamDataCounters) {
+  StreamDataCounters rtp;
+  const int64_t kStartTimeMs = 1;
+  rtp.first_packet_time_ms = kStartTimeMs;
+  rtp.packets = 1;
+  rtp.bytes = 1;
+  rtp.header_bytes = 2;
+  rtp.padding_bytes = 3;
+  EXPECT_EQ(rtp.TotalBytes(), rtp.bytes + rtp.header_bytes + rtp.padding_bytes);
+
+  StreamDataCounters rtp2;
+  rtp2.first_packet_time_ms = -1;
+  rtp2.packets = 10;
+  rtp2.bytes = 10;
+  rtp2.retransmitted_header_bytes = 4;
+  rtp2.retransmitted_bytes = 5;
+  rtp2.retransmitted_padding_bytes = 6;
+  rtp2.retransmitted_packets = 7;
+  rtp2.fec_packets = 8;
+
+  StreamDataCounters sum = rtp;
+  sum.Add(rtp2);
+  EXPECT_EQ(kStartTimeMs, sum.first_packet_time_ms);
+  EXPECT_EQ(11U, sum.packets);
+  EXPECT_EQ(11U, sum.bytes);
+  EXPECT_EQ(2U, sum.header_bytes);
+  EXPECT_EQ(3U, sum.padding_bytes);
+  EXPECT_EQ(4U, sum.retransmitted_header_bytes);
+  EXPECT_EQ(5U, sum.retransmitted_bytes);
+  EXPECT_EQ(6U, sum.retransmitted_padding_bytes);
+  EXPECT_EQ(7U, sum.retransmitted_packets);
+  EXPECT_EQ(8U, sum.fec_packets);
+  EXPECT_EQ(sum.TotalBytes(), rtp.TotalBytes() + rtp2.TotalBytes());
+
+  StreamDataCounters rtp3;
+  rtp3.first_packet_time_ms = kStartTimeMs + 10;
+  sum.Add(rtp3);
+  EXPECT_EQ(kStartTimeMs, sum.first_packet_time_ms);  // Holds oldest time.
 }
 
 TEST_F(RtpRtcpImplTest, SendsInitialNackList) {

svn2github/chromium-clone

commit 0321c0550ef04ab9d92f3373fa0037b828b6bf25

Update StreamDataCounter with FEC bytes.
Add histograms stats for send/receive FEC bitrate:
- "WebRTC.Video.FecBitrateReceivedInKbps"
- "WebRTC.Video.FecBitrateSentInKbps"

Correct media payload bytes in StreamDataCounter to not include FEC bytes.

Fix stats for rtcp packets sent/received per minute (regression from r7910).

BUG=crbug/419657
R=holmer@google.com, mflodman@webrtc.org

Review URL: https://webrtc-codereview.appspot.com/34789004

git-svn-id: http://webrtc.googlecode.com/svn/trunk@8164 4adac7df-926f-26a2-2b94-8c16560cd09d

webrtc/common_types.h
@@ -286,16 +286,16 @@ struct StreamDataCounters {
   }
 
   // Returns the number of bytes corresponding to the actual media payload (i.e.
-  // RTP headers, padding and retransmissions are excluded). Note this function
-  // does not have meaning for an RTX stream.
+  // RTP headers, padding, retransmissions and fec packets are excluded).
+  // Note this function does not have meaning for an RTX stream.
   size_t MediaPayloadBytes() const {
-    return transmitted.payload_bytes - retransmitted.payload_bytes;
+    return transmitted.payload_bytes - retransmitted.payload_bytes -
+           fec.payload_bytes;
   }
 
   int64_t first_packet_time_ms;  // Time when first packet is sent/received.
   RtpPacketCounter transmitted;  // Number of transmitted packets/bytes.
   RtpPacketCounter retransmitted;  // Number of retransmitted packets/bytes.
-  // TODO(asapersson): add FEC bytes.
   RtpPacketCounter fec;  // Number of redundancy packets/bytes.
 };
 

webrtc/modules/rtp_rtcp/interface/receive_statistics.h
@@ -61,7 +61,8 @@ class ReceiveStatistics : public Module {
                               bool retransmitted) = 0;
 
   // Increment counter for number of FEC packets received.
-  virtual void FecPacketReceived(uint32_t ssrc) = 0;
+  virtual void FecPacketReceived(const RTPHeader& header,
+                                 size_t packet_length) = 0;
 
   // Returns a map of all statisticians which have seen an incoming packet
   // during the last two seconds.
@@ -87,7 +88,8 @@ class NullReceiveStatistics : public ReceiveStatistics {
   virtual void IncomingPacket(const RTPHeader& rtp_header,
                               size_t packet_length,
                               bool retransmitted) OVERRIDE;
-  virtual void FecPacketReceived(uint32_t ssrc) OVERRIDE;
+  virtual void FecPacketReceived(const RTPHeader& header,
+                                 size_t packet_length) OVERRIDE;
   virtual StatisticianMap GetActiveStatisticians() const OVERRIDE;
   virtual StreamStatistician* GetStatistician(uint32_t ssrc) const OVERRIDE;
   virtual int64_t TimeUntilNextProcess() OVERRIDE;

webrtc/modules/rtp_rtcp/source/receive_statistics_impl.cc
@@ -200,10 +200,15 @@ void StreamStatisticianImpl::NotifyRtcpCallback() {
   rtcp_callback_->StatisticsUpdated(data, ssrc);
 }
 
-void StreamStatisticianImpl::FecPacketReceived() {
+void StreamStatisticianImpl::FecPacketReceived(const RTPHeader& header,
+                                               size_t packet_length) {
   {
     CriticalSectionScoped cs(stream_lock_.get());
     ++receive_counters_.fec.packets;
+    receive_counters_.fec.payload_bytes +=
+        packet_length - (header.headerLength + header.paddingLength);
+    receive_counters_.fec.header_bytes += header.headerLength;
+    receive_counters_.fec.padding_bytes += header.paddingLength;
   }
   NotifyRtpCallback();
 }
@@ -441,12 +446,13 @@ void ReceiveStatisticsImpl::IncomingPacket(const RTPHeader& header,
   impl->IncomingPacket(header, packet_length, retransmitted);
 }
 
-void ReceiveStatisticsImpl::FecPacketReceived(uint32_t ssrc) {
+void ReceiveStatisticsImpl::FecPacketReceived(const RTPHeader& header,
+                                              size_t packet_length) {
   CriticalSectionScoped cs(receive_statistics_lock_.get());
-  StatisticianImplMap::iterator it = statisticians_.find(ssrc);
+  StatisticianImplMap::iterator it = statisticians_.find(header.ssrc);
   // Ignore FEC if it is the first packet.
   if (it != statisticians_.end()) {
-    it->second->FecPacketReceived();
+    it->second->FecPacketReceived(header, packet_length);
   }
 }
 
@@ -543,7 +549,8 @@ void NullReceiveStatistics::IncomingPacket(const RTPHeader& rtp_header,
                                            size_t packet_length,
                                            bool retransmitted) {}
 
-void NullReceiveStatistics::FecPacketReceived(uint32_t ssrc) {}
+void NullReceiveStatistics::FecPacketReceived(const RTPHeader& header,
+                                              size_t packet_length) {}
 
 StatisticianMap NullReceiveStatistics::GetActiveStatisticians() const {
   return StatisticianMap();

webrtc/modules/rtp_rtcp/source/receive_statistics_impl.h
@@ -44,7 +44,7 @@ class StreamStatisticianImpl : public StreamStatistician {
   void IncomingPacket(const RTPHeader& rtp_header,
                       size_t packet_length,
                       bool retransmitted);
-  void FecPacketReceived();
+  void FecPacketReceived(const RTPHeader& header, size_t packet_length);
   void SetMaxReorderingThreshold(int max_reordering_threshold);
   void ProcessBitrate();
   virtual void LastReceiveTimeNtp(uint32_t* secs, uint32_t* frac) const;
@@ -110,7 +110,8 @@ class ReceiveStatisticsImpl : public ReceiveStatistics,
   virtual void IncomingPacket(const RTPHeader& header,
                               size_t packet_length,
                               bool retransmitted) OVERRIDE;
-  virtual void FecPacketReceived(uint32_t ssrc) OVERRIDE;
+  virtual void FecPacketReceived(const RTPHeader& header,
+                                 size_t packet_length) OVERRIDE;
   virtual StatisticianMap GetActiveStatisticians() const OVERRIDE;
   virtual StreamStatistician* GetStatistician(uint32_t ssrc) const OVERRIDE;
   virtual void SetMaxReorderingThreshold(int max_reordering_threshold) OVERRIDE;

webrtc/modules/rtp_rtcp/source/rtp_sender.cc
@@ -900,6 +900,10 @@ void RTPSender::UpdateRtpStats(const uint8_t* buffer,
   }
   if (IsFecPacket(buffer, header)) {
     ++counters->fec.packets;
+    counters->fec.payload_bytes +=
+        packet_length - (header.headerLength + header.paddingLength);
+    counters->fec.header_bytes += header.headerLength;
+    counters->fec.padding_bytes += header.paddingLength;
   }
 
   if (is_retransmit) {

webrtc/video_engine/vie_channel.cc
@@ -202,11 +202,11 @@ void ViEChannel::UpdateHistograms() {
     int64_t elapsed_sec = rtcp_received.TimeSinceFirstPacketInMs(now) / 1000;
     if (elapsed_sec > metrics::kMinRunTimeInSeconds) {
       RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.NackPacketsReceivedPerMinute",
-          rtcp_received.nack_packets / elapsed_sec / 60);
+          rtcp_received.nack_packets * 60 / elapsed_sec);
       RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.FirPacketsReceivedPerMinute",
-          rtcp_received.fir_packets / elapsed_sec / 60);
+          rtcp_received.fir_packets * 60 / elapsed_sec);
       RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.PliPacketsReceivedPerMinute",
-          rtcp_received.pli_packets / elapsed_sec / 60);
+          rtcp_received.pli_packets * 60 / elapsed_sec);
       if (rtcp_received.nack_requests > 0) {
         RTC_HISTOGRAM_PERCENTAGE(
             "WebRTC.Video.UniqueNackRequestsReceivedInPercent",
@@ -224,11 +224,11 @@ void ViEChannel::UpdateHistograms() {
     int64_t elapsed_sec = rtcp_sent.TimeSinceFirstPacketInMs(now) / 1000;
     if (elapsed_sec > metrics::kMinRunTimeInSeconds) {
       RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.NackPacketsSentPerMinute",
-          rtcp_sent.nack_packets / elapsed_sec / 60);
+          rtcp_sent.nack_packets * 60 / elapsed_sec);
       RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.FirPacketsSentPerMinute",
-          rtcp_sent.fir_packets / elapsed_sec / 60);
+          rtcp_sent.fir_packets * 60 / elapsed_sec);
       RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.PliPacketsSentPerMinute",
-          rtcp_sent.pli_packets / elapsed_sec / 60);
+          rtcp_sent.pli_packets * 60 / elapsed_sec);
       if (rtcp_sent.nack_requests > 0) {
         RTC_HISTOGRAM_PERCENTAGE("WebRTC.Video.UniqueNackRequestsSentInPercent",
             rtcp_sent.UniqueNackRequestsInPercent());
@@ -261,6 +261,10 @@ void ViEChannel::UpdateHistograms() {
         RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.RtxBitrateReceivedInKbps",
             rtx.transmitted.TotalBytes() * 8 / elapsed_sec / 1000);
       }
+      if (vie_receiver_.IsFecEnabled()) {
+        RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.FecBitrateReceivedInKbps",
+            rtp_rtx.fec.TotalBytes() * 8 / elapsed_sec / 1000);
+      }
     }
   }
 }
@@ -289,6 +293,14 @@ void ViEChannel::UpdateHistogramsAtStopSend() {
   if (vie_receiver_.GetRtxSsrc(&ssrc)) {
     RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.RtxBitrateSentInKbps",
         rtx.transmitted.TotalBytes() * 8 / elapsed_sec / 1000);
+  }
+  bool fec_enabled = false;
+  uint8_t pltype_red;
+  uint8_t pltype_fec;
+  rtp_rtcp_->GenericFECStatus(fec_enabled, pltype_red, pltype_fec);
+  if (fec_enabled) {
+    RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.FecBitrateSentInKbps",
+        rtp_rtx.fec.TotalBytes() * 8 / elapsed_sec / 1000);
   }
 }
 

webrtc/video_engine/vie_receiver.cc
@@ -129,6 +129,10 @@ bool ViEReceiver::GetRtxSsrc(uint32_t* ssrc) const {
   return rtp_payload_registry_->GetRtxSsrc(ssrc);
 }
 
+bool ViEReceiver::IsFecEnabled() const {
+  return rtp_payload_registry_->ulpfec_payload_type() > -1;
+}
+
 uint32_t ViEReceiver::GetRemoteSsrc() const {
   return rtp_receiver_->SSRC();
 }
@@ -319,7 +323,7 @@ bool ViEReceiver::ParseAndHandleEncapsulatingHeader(const uint8_t* packet,
   if (rtp_payload_registry_->IsRed(header)) {
     int8_t ulpfec_pt = rtp_payload_registry_->ulpfec_payload_type();
     if (packet[header.headerLength] == ulpfec_pt)
-      rtp_receive_statistics_->FecPacketReceived(header.ssrc);
+      rtp_receive_statistics_->FecPacketReceived(header, packet_length);
     if (fec_receiver_->AddReceivedRedPacket(
             header, packet, packet_length, ulpfec_pt) != 0) {
       return false;

webrtc/video_engine/vie_receiver.h
@@ -51,6 +51,8 @@ class ViEReceiver : public RtpData {
   void SetRtxSsrc(uint32_t ssrc);
   bool GetRtxSsrc(uint32_t* ssrc) const;
 
+  bool IsFecEnabled() const;
+
   uint32_t GetRemoteSsrc() const;
   int GetCsrcs(uint32_t* csrcs) const;
 

webrtc/modules/rtp_rtcp/source/receive_statistics_unittest.cc
@@ -276,6 +276,9 @@ class RtpTestCallback : public StreamDataCountersCallback {
     EXPECT_EQ(expected.retransmitted.padding_bytes,
               stats_.retransmitted.padding_bytes);
     EXPECT_EQ(expected.retransmitted.packets, stats_.retransmitted.packets);
+    EXPECT_EQ(expected.fec.payload_bytes, stats_.fec.payload_bytes);
+    EXPECT_EQ(expected.fec.header_bytes, stats_.fec.header_bytes);
+    EXPECT_EQ(expected.fec.padding_bytes, stats_.fec.padding_bytes);
     EXPECT_EQ(expected.fec.packets, stats_.fec.packets);
   }
 
@@ -336,13 +339,16 @@ TEST_F(ReceiveStatisticsTest, RtpCallbacks) {
   header1_.paddingLength = 0;
   ++header1_.sequenceNumber;
   clock_.AdvanceTimeMilliseconds(5);
-  // One recovered packet.
+  // One FEC packet.
   receive_statistics_->IncomingPacket(
       header1_, kPacketSize1 + kHeaderLength, false);
-  receive_statistics_->FecPacketReceived(kSsrc1);
+  receive_statistics_->FecPacketReceived(header1_,
+                                         kPacketSize1 + kHeaderLength);
   expected.transmitted.payload_bytes = kPacketSize1 * 4;
   expected.transmitted.header_bytes = kHeaderLength * 4;
   expected.transmitted.packets = 4;
+  expected.fec.payload_bytes = kPacketSize1;
+  expected.fec.header_bytes = kHeaderLength;
   expected.fec.packets = 1;
   callback.Matches(5, kSsrc1, expected);
 
@@ -361,12 +367,13 @@ TEST_F(ReceiveStatisticsTest, RtpCallbacksFecFirst) {
   receive_statistics_->RegisterRtpStatisticsCallback(&callback);
 
   const uint32_t kHeaderLength = 20;
+  header1_.headerLength = kHeaderLength;
 
   // If first packet is FEC, ignore it.
-  receive_statistics_->FecPacketReceived(kSsrc1);
+  receive_statistics_->FecPacketReceived(header1_,
+                                         kPacketSize1 + kHeaderLength);
   EXPECT_EQ(0u, callback.num_calls_);
 
-  header1_.headerLength = kHeaderLength;
   receive_statistics_->IncomingPacket(
       header1_, kPacketSize1 + kHeaderLength, false);
   StreamDataCounters expected;
@@ -377,7 +384,10 @@ TEST_F(ReceiveStatisticsTest, RtpCallbacksFecFirst) {
   expected.fec.packets = 0;
   callback.Matches(1, kSsrc1, expected);
 
-  receive_statistics_->FecPacketReceived(kSsrc1);
+  receive_statistics_->FecPacketReceived(header1_,
+                                         kPacketSize1 + kHeaderLength);
+  expected.fec.payload_bytes = kPacketSize1;
+  expected.fec.header_bytes = kHeaderLength;
   expected.fec.packets = 1;
   callback.Matches(2, kSsrc1, expected);
 }